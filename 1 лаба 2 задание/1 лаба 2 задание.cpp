96d7b89d0e421fa5e376aae4cc3e8261b77ef068
Ilia Kovalev <kovalev.ilia0505@gmail.com>
2022-10-14 20:00:33 +0300

    laba2

---
+++ 1 лаба 2 задание/1 лаба 2 задание.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <locale.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int user_a;
+	user_a = 0;
+	cout << "Введите значение a \n";
+	cin >> user_a;
+	if (user_a > -120 && user_a < -100)
+	{
+		cout << "Число " << user_a << " относится к группе A\n\n\n\n\n";
+	}
+	else if (user_a > 100 && user_a % 5 == 0)
+	{
+		cout << "Число " << user_a << " относится к группе B\n\n\n\n\n";
+	}
+	else if (user_a < 0 && user_a % 2 == 0)
+	{
+		cout << "Число " << user_a << " относится к группе C\n\n\n\n\n";
+	}
+	else
+		cout << "Число " << user_a << " относится к группе D\n\n\n\n\n";
+
+
+}
+
